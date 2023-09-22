4e6d9e7d0cba48b3661217a3f666ab088ee10278
dadiaomengmeizi <144116443+dadiaomengmeizi@users.noreply.github.com>
2023-09-22 15:50:02 +0800

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,56 @@
+key: "DSL-PB17"
+val: {
+    "Motherboard": {
+        "sleep_power": {
+            "pre_detect_wait_time": 5000,
+            "pre_detect_sample_num": 3,
+            "def_test_sampling_time": 5000,
+            "re_test_sampling_time": 8000,
+            "min": 10,
+            "max": 500,
+            "mid": 65
+        }
+    },
+    "Finger": {
+        "sleep_power": {
+            "pre_detect_wait_time": 5000,
+            "pre_detect_sample_num": 3,
+            "def_test_sampling_time": 3000,
+            "re_test_sampling_time": 5000,
+            "min": 10,
+            "max": 20,
+            "mid": 18
+        },
+        "work_power": {
+            "pre_detect_wait_time": 5000,
+            "pre_detect_sample_num": 3,
+            "def_test_sampling_time": 3000,
+            "re_test_sampling_time": 5000,
+            "min": 22000,
+            "max": 40000,
+            "mid": 38000
+        }
+    },
+    "ZB": {
+        "sleep_power": {
+            "pre_detect_wait_time": 5000,
+            "pre_detect_sample_num": 3,
+            "def_test_sampling_time": 3000,
+            "re_test_sampling_time": 5000,
+            "min": 51,
+            "max": 53,
+            "mid": 52
+        }
+    },
+    "Touch": {
+        "sleep_power": {
+            "pre_detect_wait_time": 5000,
+            "pre_detect_sample_num": 3,
+            "def_test_sampling_time": 3000,
+            "re_test_sampling_time": 5000,
+            "min": 5,
+            "max": 120,
+            "mid": 25
+        }
+    }
+}
